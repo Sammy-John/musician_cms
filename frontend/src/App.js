674084b2a427eecf295a8237d09171f22e6bbaf0
nabutech <nabutech@proton.me>
2024-12-19 12:57:46 +1300

    Added Components/Pages and App.js and Index.js setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+// Import Pages
+import Home from "./pages/Home";
+import NewsList from "./pages/NewsList";
+import NewsItemPage from "./pages/NewsItemPage";
+import Music from "./pages/Music";
+import Videos from "./pages/Videos";
+import Images from "./pages/Images";
+import Contact from "./pages/Contact";
+
+
+// Import Layout Components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Header */}
+      <Header />
+
+      {/* Page Routes */}
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<NewsList />} />
+          <Route path="/news/:id" element={<NewsItemPage />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/videos" element={<Videos />} />
+          <Route path="/images" element={<Images />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        {/* Footer */}
+        <Footer />
     </div>
+    </Router>
   );
 }
 
