dd8134557d866f1119a378b810b25acddb91f408
nabutech <nabutech@proton.me>
2024-12-19 15:23:12 +1300

    Gig Dates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import News from "../components/News";
+import Gigs from "../components/Gigs";
 
 const Home = () => {
   return (
     <div>
       <News />
+      <Gigs />
     </div>
   );
 };
